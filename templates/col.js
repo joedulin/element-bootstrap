4510402535b1db38f9c972fdb6dafa36b97b8fe1
joedulin <joedulin@gmail.com>
2013-12-12 19:19:33 -0700

    Working on adding more error checking for setSize and similar

--- templates/col.js
+++ templates/col.js
@@ -1,18 +1,22 @@
 var Template = require('elementfw').Template;
 var e = require('element.js').e;
 
-var col = new Template();
-col.body = e.div();
-
-col.setSize = function (size, num) {
+Template.prototype.setSize = function (size, num) {
 	if (typeof size === 'object') {
 		for (s in size) {
-			col.body.setSize(s,size[s]);
+			col.setSize(s,size[s]);
 		}
 	} else {
 		switch (size) {
 			case 'all':
-				col.body.addClass([
+				var remClasses = [];
+				for (var i=0; i<this.body.classes.length; i++) {
+					if (this.body.classes[i].search(/^col....[0-9]/) > -1) {
+						remClasses.push(this.body.classes[i]);
+					}
+				}
+				this.body.removeClass(remClasses);
+				this.body.addClass([
 					'col-xs-' + num,
 					'col-sm-' + num,
 					'col-md-' + num,
@@ -20,45 +24,45 @@ col.setSize = function (size, num) {
 				]);
 				break;
 			case 'phone':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-' + num,
 					'col-sm-' + num
 				]);
 				break;
 			case 'desktop':
-				col.body.addClass([
+				this.body.addClass([
 					'col-md-' + num,
 					'col-lg-' + num
 				]);
 				break;
 			case 'xs':
-				col.body.addClass('col-xs-' + num);
+				this.body.addClass('col-xs-' + num);
 				break;
 			case 'sm':
-				col.body.addClass('col-sm-' + num);
+				this.body.addClass('col-sm-' + num);
 				break;
 			case 'md':
-				col.body.addClass('col-md-' + num);
+				this.body.addClass('col-md-' + num);
 				break;
 			case 'lg':
-				col.body.addClass('col-lg-' + num);
+				this.body.addClass('col-lg-' + num);
 				break;
 			default:
-				col.body.addClass('col-md-' + num);
+				this.body.addClass('col-md-' + num);
 				break;
 		}
 	}
 };
 
-col.setOffset = function (size, num) {
+Template.prototype.setOffset = function (size, num) {
 	if (typeof size === 'object') {
 		for (s in size) {
-			col.body.setOffset(s,size[s]);
+			this.body.setOffset(s,size[s]);
 		}
 	} else {
 		switch (size) {
 			case 'all':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-offset-' + num,
 					'col-sm-offset-' + num,
 					'col-md-offset-' + num,
@@ -66,45 +70,45 @@ col.setOffset = function (size, num) {
 				]);
 				break;
 			case 'phone':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-offset-' + num,
 					'col-sm-offset-' + num
 				]);
 				break;
 			case 'desktop':
-				col.body.addClass([
+				this.body.addClass([
 					'col-md-offset-' + num,
 					'col-lg-offset-' + num
 				]);
 				break;
 			case 'xs':
-				col.body.addClass('col-xs-offset-' + num);
+				this.body.addClass('col-xs-offset-' + num);
 				break;
 			case 'sm':
-				col.body.addClass('col-sm-offset-' + num);
+				this.body.addClass('col-sm-offset-' + num);
 				break;
 			case 'md':
-				col.body.addClass('col-md-offset-' + num);
+				this.body.addClass('col-md-offset-' + num);
 				break;
 			case 'lg':
-				col.body.addClass('col-lg-offset-' + num);
+				this.body.addClass('col-lg-offset-' + num);
 				break;
 			default:
-				col.body.addClass('col-md-offset-' + num);
+				this.body.addClass('col-md-offset-' + num);
 				break;
 		}
 	}
 };
 
-col.setPush = function (size, num) {
+Template.prototype.setPush = function (size, num) {
 	if (typeof size === 'object') {
 		for (s in size) {
-			col.body.setPush(s,size[s]);
+			this.body.setPush(s,size[s]);
 		}
 	} else {
 		switch (size) {
 			case 'all':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-push-' + num,
 					'col-sm-push-' + num,
 					'col-md-push-' + num,
@@ -112,45 +116,45 @@ col.setPush = function (size, num) {
 				]);
 				break;
 			case 'phone':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-push-' + num,
 					'col-sm-push-' + num
 				]);
 				break;
 			case 'desktop':
-				col.body.addClass([
+				this.body.addClass([
 					'col-md-push-' + num,
 					'col-lg-push-' + num
 				]);
 				break;
 			case 'xs':
-				col.body.addClass('col-xs-push-' + num);
+				this.body.addClass('col-xs-push-' + num);
 				break;
 			case 'sm':
-				col.body.addClass('col-sm-push-' + num);
+				this.body.addClass('col-sm-push-' + num);
 				break;
 			case 'md':
-				col.body.addClass('col-md-push-' + num);
+				this.body.addClass('col-md-push-' + num);
 				break;
 			case 'lg':
-				col.body.addClass('col-lg-push-' + num);
+				this.body.addClass('col-lg-push-' + num);
 				break;
 			default:
-				col.body.addClass('col-md-push-' + num);
+				this.body.addClass('col-md-push-' + num);
 				break;
 		}
 	}
 };
 
-col.setPull = function (size, num) {
+Template.prototype.setPull = function (size, num) {
 	if (typeof size === 'object') {
 		for (s in size) {
-			col.body.setPull(s,size[s]);
+			this.body.setPull(s,size[s]);
 		}
 	} else {
 		switch (size) {
 			case 'all':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-pull-' + num,
 					'col-sm-pull-' + num,
 					'col-md-pull-' + num,
@@ -158,34 +162,37 @@ col.setPull = function (size, num) {
 				]);
 				break;
 			case 'phone':
-				col.body.addClass([
+				this.body.addClass([
 					'col-xs-pull-' + num,
 					'col-sm-pull-' + num
 				]);
 				break;
 			case 'desktop':
-				col.body.addClass([
+				this.body.addClass([
 					'col-md-pull-' + num,
 					'col-lg-pull-' + num
 				]);
 				break;
 			case 'xs':
-				col.body.addClass('col-xs-pull-' + num);
+				this.body.addClass('col-xs-pull-' + num);
 				break;
 			case 'sm':
-				col.body.addClass('col-sm-pull-' + num);
+				this.body.addClass('col-sm-pull-' + num);
 				break;
 			case 'md':
-				col.body.addClass('col-md-pull-' + num);
+				this.body.addClass('col-md-pull-' + num);
 				break;
 			case 'lg':
-				col.body.addClass('col-lg-pull-' + num);
+				this.body.addClass('col-lg-pull-' + num);
 				break;
 			default:
-				col.body.addClass('col-md-pull-' + num);
+				this.body.addClass('col-md-pull-' + num);
 				break;
 		}
 	}
 };
+
+var col = new Template();
+col.body = e.div();
 
 exports.Template = col;
